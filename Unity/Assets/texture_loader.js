a5346d591eb26ce76d920a7a557d64702ea5ff95
Alex Leiva <alx.leiva@gmail.com>
2014-09-01 16:16:24 +1000

    Updated unity project

---
+++ Unity/Assets/texture_loader.js
@@ -0,0 +1,14 @@
+﻿#pragma strict
+
+var url = "file:///X:/Development/Oculus Rift/Tuscany_HC.jpg";
+
+function Start () {
+     // Start a download of the given URL
+    var www : WWW = new WWW (url);
+ 
+    // Wait for download to complete
+    yield www;
+ 
+    // assign texture
+    renderer.material.mainTexture = www.texture; 
+}
